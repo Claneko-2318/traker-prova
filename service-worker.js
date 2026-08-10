f77de224c6c64a128764788fb75f4fb345b7ab9f
Claneko <clanekocolucci@gmail.com>
2026-08-10 11:49:21 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tracker-lavoro-cafe-v8-4-layout-notes";
+const CACHE_NAME = "tracker-lavoro-cafe-v8-6-task-containers";
 const APP_SHELL = [
   "./working-tracker.html",
   "./kurorei-chill.png",
